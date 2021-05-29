78ff518ae411de43c062d4c8dee5f8a0f257ddf5
CyclingFoodmanPA <ks.safford@verizon.net>
2021-05-29 14:52:44 -0400

    Added DB Script

---
+++ jQuery_MVC_CRUD/DBScript/CRUD_jQuery_MVC.sql
@@ -0,0 +1,27 @@
+USE master;
+GO
+
+DROP DATABASE IF EXISTS AjaxSamples;
+GO
+
+CREATE DATABASE AjaxSamples;
+GO
+
+USE AjaxSamples;
+GO
+
+CREATE TABLE dbo.Customer (
+	CustomerId INT IDENTITY(1,1) NOT NULL
+	, CustomerName VARCHAR(40) NOT NULL
+	, Country VARCHAR(40) NOT NULL
+	, CONSTRAINT PK_Customer_CustomerID PRIMARY KEY CLUSTERED (CustomerId)
+);
+GO
+
+
+INSERT INTO dbo.Customer (CustomerName, Country)
+VALUES ('John Hammond', 'United States')
+	, ('Kieth LaTray', 'United States')
+	, ('Prabu Ramaswammy', 'India')
+	, ('Ivan Kossak', 'Russia')
+	, ('Itzik Ben-gan', 'Israel');
