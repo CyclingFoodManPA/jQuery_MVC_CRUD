f7ddd3dc9a8e329648a766cef749ffc89e95d758
CyclingFoodmanPA <ks.safford@verizon.net>
2021-05-29 15:07:10 -0400

    Added CRUD operations to controller and modified Index.cshtml file to be able to use AJAX and JSON for CRUD operations.

--- jQuery_MVC_CRUD/Controllers/HomeController.cs
+++ jQuery_MVC_CRUD/Controllers/HomeController.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿// https://www.aspsnippets.com/Articles/MVC-jQuery-CRUD-Select-Insert-Edit-Update-and-Delete-using-jQuery-AJAX-and-JSON-in-ASPNet-MVC.aspx
+
+#region usings
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using jQuery_MVC_CRUD.Context;
+
+#endregion usings
 
 namespace jQuery_MVC_CRUD.Controllers
 {
@@ -10,21 +17,53 @@ namespace jQuery_MVC_CRUD.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            CustomerEntities _db = new CustomerEntities();
+            List<Customer> customers = _db.Customers.ToList();
+
+            return View(customers);
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public JsonResult InsertCustomer(Customer customer)
         {
-            ViewBag.Message = "Your application description page.";
+            using (CustomerEntities entities = new CustomerEntities())
+            {
+                entities.Customers.Add(customer);
+                entities.SaveChanges();
+            }
 
-            return View();
+            return Json(customer);
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult UpdateCustomer(Customer customer)
         {
-            ViewBag.Message = "Your contact page.";
+            using (CustomerEntities entities = new CustomerEntities())
+            {
+                Customer updatedCustomer = (from c in entities.Customers
+                    where c.CustomerId == customer.CustomerId
+                    select c).FirstOrDefault();
+                updatedCustomer.Name = customer.Name;
+                updatedCustomer.Country = customer.Country;
+                entities.SaveChanges();
+            }
 
-            return View();
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteCustomer(int customerId)
+        {
+            using (CustomerEntities entities = new CustomerEntities())
+            {
+                Customer customer = (from c in entities.Customers
+                    where c.CustomerId == customerId
+                    select c).FirstOrDefault();
+                entities.Customers.Remove(customer);
+                entities.SaveChanges();
+            }
+
+            return new EmptyResult();
         }
     }
 }
